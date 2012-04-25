8e43b7a00d0d7fe2ad14018fad8006950cd0dc26
Martin Rusev <martinrusev@zoho.com>
2012-04-25 11:34:31 +0300

    Benchmark updates

--- benchmark.py
+++ benchmark.py
@@ -5,44 +5,18 @@ import zmq
 import urllib2
 import json
 
-runs = 10000
+runs = 5000
 http_address = 'http://127.0.0.1:2464'
+zeromq_address = '127.0.0.1:5464'
 
-amonpy.config.address = '127.0.0.1:5464'
-amonpy.config.protocol = 'zeromq'
+zeromq_bench = True
+http_bench = True
+standart_bench = True
 
-start = time.time()
-for i in range(0, runs):
-    amonpy.log({"key": "value"})
-
-print "ZeroMQ - {0}".format(time.time()-start) 
-
-amonpy.config.address = http_address
-amonpy.config.protocol = 'http'
-
-start = time.time()
-for i in range(0, runs):
-    amonpy.log({"key": "value"})
-
-print "HTTP - {0}".format(time.time()-start) 
-
-logger = logging.getLogger('amonpy_application')
-logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler('bench.log')
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(message)s')
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
-start = time.time()
-for i in range(0, runs):
-    _dict = {"key": "value"}
-    logger.info(str(_dict))
-
-print "Standard logging - {0}".format(time.time()-start) 
+print "Runs: {0}".format(runs)
 
 def post_zeromq():
-    address = "tcp://127.0.0.1:5464"
+    address = "tcp://{0}".format(zeromq_address)
     data = {"type": 'test', "content" : 'test'}
     context = zmq.Context.instance()
     
@@ -50,23 +24,66 @@ def post_zeromq():
     socket.connect(address)
     socket.send_json(data, zmq.NOBLOCK)
     context.destroy()
+
+if zeromq_bench is True:
+    amonpy.config.address = zeromq_address
+    amonpy.config.protocol = 'zeromq'
+
+
+    start = time.time()
+    for i in range(0, runs):
+        amonpy.log({"key": "value"})
+
+    print "ZeroMQ - {0}".format(time.time()-start) 
     
-start = time.time()
-for i in range(0, runs):
-    post_zeromq()
+    
+    start = time.time()
+    for i in range(0, runs):
+        post_zeromq()
 
-print "ZeroMQ Raw - {0}".format(time.time()-start) 
+    print "ZeroMQ Raw - {0}".format(time.time()-start) 
+
+
+if http_bench is True:
+    amonpy.config.address = http_address
+    amonpy.config.protocol = 'http'
+
+    start = time.time()
+    for i in range(0, runs):
+        amonpy.log({"key": "value"})
+
+    print "HTTP - {0}".format(time.time()-start) 
+
+
+    start = time.time()
+    for i in range(0, runs):
+        data = {'message': 'test me', 'tags':['debug','test']}
+        json_string = json.dumps(data)
+
+        req = urllib2.Request("{0}/api/log".format(http_address),
+                          headers = {"Content-Type": "application/json"},
+                          data = json_string)
+        f = urllib2.urlopen(req)
+
+    print "HTTP Raw - {0}".format(time.time()-start) 
+
+
+if standart_bench is True:
+    logger = logging.getLogger('amonpy_application')
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('bench.log')
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    start = time.time()
+    for i in range(0, runs):
+        _dict = {"key": "value"}
+        logger.info(str(_dict))
+
+    print "Standard logging - {0}".format(time.time()-start) 
+
 
     
-start = time.time()
-for i in range(0, runs):
-    data = {'message': 'test me', 'tags':['debug','test']}
-    json_string = json.dumps(data)
-
-    req = urllib2.Request("{0}/api/log".format(http_address),
-                      headers = {"Content-Type": "application/json"},
-                      data = json_string)
-    f = urllib2.urlopen(req)
-
-print "HTTP Raw - {0}".format(time.time()-start) 
 
